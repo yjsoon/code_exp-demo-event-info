1efd88d5cd2aa3a415aaf8e3064b1735cb04dcda
YJ Soon <yjsoon@tinkertanker.com>
2021-06-08 15:56:31 +0800

    Added Events Second Screen, win $1MMMMMM

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-function EventsScreen() {
+function EventsScreen({ navigation }) {
   return (
     <View
       style={{
@@ -13,8 +13,16 @@ function EventsScreen() {
       }}
     >
       <Text>Events!</Text>
+      <Button
+        title="You've won $1MM!!!! CLICK HERE!!!"
+        onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
     </View>
   );
+}
+
+function EventsSecondScreen() {
+  return <Text>Don't get scammed</Text>;
 }
 
 const Stack = createStackNavigator();
@@ -23,6 +31,7 @@ export default function EventsStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Events" component={EventsScreen} />
+      <Stack.Screen name="Don't Get Scammed" component={EventsSecondScreen} />
     </Stack.Navigator>
   );
 }
